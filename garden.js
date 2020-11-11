8487bcb8fb1e31f6556ebda36b07956cf880b321
hannaelarsson93 <73796481+hannaelarsson93@users.noreply.github.com>
2020-11-11 17:57:54 +0100

    test

--- garden.js
+++ garden.js
@@ -86,28 +86,6 @@ function clearFields() {
  }
  
 
-// Code to randomly place image on the webpage 
-
-// function getRandomPosition(element) {
-//   var x = document.body.offsetHeight-element.clientHeight;
-//   var y = document.body.offsetWidth-element.clientWidth;
-//   var randomX = Math.floor(Math.random()*x);
-//   var randomY = Math.floor(Math.random()*y);
-//   return [randomX,randomY];
-  
-// }
-// function random(){
-//   var image = document.createElement('img');
-//   img.setAttribute("style", "position:absolute;");
-//   img.setAttribute("src", "https://assets10.lottiefiles.com/packages/lf20_xzIOCI.json");
-
-//   document.body.appendChild(img);
-//   var xy = getRandomPosition(img);
-//   img.style.top = xy[0] + 'px';
-//   img.style.left = xy[1] + 'px';
-// }
-
-
 
 
 
